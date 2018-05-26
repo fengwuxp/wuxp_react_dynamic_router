169c4468544832d39a89f74f6fe7f1b6851e73b7
风 <1109695647@qq.com>
2018-05-26 15:41:00 +0800

    Signed-off-by: 风 <1109695647@qq.com>

--- types/index.d.ts
+++ types/index.d.ts
@@ -11,3 +11,13 @@ declare module "*.jpeg";
 declare module "*.gif";
 
 declare module "*.svg";
+
+declare module "*.ttf";
+
+declare module "*.woff";
+
+declare module "*.woff2";
+
+declare module "*.eot";
+
+declare module "*.json"
