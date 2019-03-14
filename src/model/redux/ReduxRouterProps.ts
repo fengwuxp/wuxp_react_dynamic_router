ab894b2c4d30d85e124cfd5300cc1b3d8f92b479
风 <1109695647@qq.com>
2019-03-14 16:28:41 +0800

    Signed-off-by: 风 <1109695647@qq.com>

--- src/model/redux/ReduxRouterProps.ts
+++ src/model/redux/ReduxRouterProps.ts
@@ -1,11 +1,14 @@
 import {History, Location} from "history";
 import {match} from "react-router";
-import {DispatchProp} from "react-redux";
+import {Dispatch} from "react-redux";
 
 /**
  * redux react-router-redux 会分发到子组件的props
  */
-export interface ReduxRouterProps extends DispatchProp<any> {
+export interface ReduxRouterProps<S = any> {
+
+
+    dispatch?: Dispatch<S>;
 
     location?: Location,
 
